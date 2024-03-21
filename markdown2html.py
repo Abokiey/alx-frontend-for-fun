e8a22123466a60623e0686cf89689cb18fe96a44
Abokiey <sweneysam@gmail.com>
2024-03-21 13:54:33 +0300

    0

---
+++ markdown2html.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""an argument with first argument a markdown and the second
+    argument is the output file name
+"""
+
+
+import sys
+import os.path
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py README.md README.html")
+        sys.exit(1)
+        
+    if not os.path.isfile(sys.argv[1]):
+        print("Missing <filename>")
+        sys.exit(1)
+    else:
+        exit(0)
