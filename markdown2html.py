8c3cfdda871f637885923bc43add1a2b5c0b788d
Abokiey <sweneysam@gmail.com>
2024-03-21 17:14:50 +0300

    1

--- markdown2html.py
+++ markdown2html.py
@@ -6,6 +6,7 @@
 
 import sys
 import os.path
+import markdown2
 
 if __name__ == "__main__":
     """run the code"""
@@ -16,5 +17,21 @@ if __name__ == "__main__":
     if not os.path.isfile(sys.argv[1]):
         print("Missing {}".format(sys.argv[1]), file=sys.stderr)
         sys.exit(1)
+        
+    with open(sys.argv[1]) as read:
+        with open(sys.argv[2], 'w') as html:
+            for line in read:
+                length = len(line)
+                heading = line.lstrip('#')
+                heading_no = length - len(heading)
+                
+                if 1 <= heading_no <= 6:
+                    line = '<h{}>'.format(
+                        heading_no) + heading.strip() + '</h{}>\n'.format(
+                        heading_no)
+                        
+                if length > 1:
+                    html.write(line)
+            
     
     exit(0)
