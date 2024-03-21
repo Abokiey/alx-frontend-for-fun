a20bf86de7b767f9bd5460d43376de3925c50ae2
Abokiey <sweneysam@gmail.com>
2024-03-21 15:48:08 +0300

    0

--- markdown2html.py
+++ markdown2html.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         sys.exit(1)
         
     if not os.path.isfile(sys.argv[1]):
-        print(f"Missing {sys.argv[1]}")
+        print("Missing {}".format(sys.argv[1]), file=sys.stderr)
         sys.exit(1)
     
     exit(0)
